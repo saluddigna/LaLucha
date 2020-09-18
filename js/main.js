f96d907b78539ab0bd721894d206a3c261136485
HiperEmmanuel <hiperemmanuel@gmail.com>
2020-09-18 10:01:33 -0600

    Seleccion de paginas

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
   $('#banner').load('../modelos/banner.html');
   $('#nav').load('../modelos/navbar.html');
-  
+  $('#seccion').load('../modelos/cita.html');
   
 });
 
 function seccion(nav){
   if(nav==1){
-    alert( "Handler for .click() called." );
+    $('#seccion').load('../modelos/cita.html');
+    removerClaseNav();
+    $("#btnRegistro").addClass("active");
   }else if(nav==2){
-    
+    $('#seccion').load('../modelos/revista.html');
+    removerClaseNav();
+    $("#btnRevista").addClass("active");
   }else if(nav==3){
-    
+    $('#seccion').load('../modelos/beneficiados.html');
+    removerClaseNav();
+    $("#btnPxBeneficiados").addClass("active");
   }else{
-    
+    $('#seccion').load('../modelos/logIn.html');
+    removerClaseNav();
+    $("#btnLogIn").addClass("active");
   }
+}
+function removerClaseNav(){
+  $("#btnRegistro").removeClass("active");
+  $("#btnRevista").removeClass("active");
+  $("#btnPxBeneficiados").removeClass("active");
+  $("#btnLogIn").removeClass("active");
 }
