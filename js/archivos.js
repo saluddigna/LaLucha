824bed80994c06c5631bdf2f1bb775b93f0280a5
HiperEmmanuel <hiperemmanuel@gmail.com>
2020-09-19 14:07:31 -0600

    Login primerPantalla

--- js/archivos.js
+++ js/archivos.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
     $('#infoCita').load('./modelos/componentes/infoCita.html');
     $('#formCita').load('./modelos/componentes/formCita.html');
+    $('#ingresar').load('./modelos/componentes/ingresar.html');
 });
