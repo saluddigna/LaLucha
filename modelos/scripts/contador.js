b15d57c594dca71a8ce5a2c0662604667ba655fe
HiperEmmanuel <hiperemmanuel@gmail.com>
2020-10-20 10:15:06 -0600

    actualizacion

--- modelos/scripts/contador.js
+++ modelos/scripts/contador.js
@@ -1,12 +1,12 @@
 function startSocket(bandera) {
-    let data = {
-        query: {}
-    }
-    var socket = io("https://socket-lucha.herokuapp.com/", data)
-    socket.on('MastoGratis', function (payload) {
-        console.log((payload))
-        // citasGratis = payload.disponibles
-        $("#mastosEntregadas").text((payload.numeroMasto).toString())
-        $("#mastoEntregar").text((payload.MastosAEntregar).toString())
-    })
+    // let data = {
+    //     query: {}
+    // }
+    // var socket = io("https://socket-lucha.herokuapp.com/", data)
+    // socket.on('MastoGratis', function (payload) {
+    //     console.log((payload))
+    //     // citasGratis = payload.disponibles
+    //     $("#mastosEntregadas").text((payload.numeroMasto).toString())
+    //     $("#mastoEntregar").text((payload.MastosAEntregar).toString())
+    // })
 }
